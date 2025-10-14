5325e3051c1df2a6538b2ce575a143bf5d0efc68
TheshikaSamaraweera <theshikanavod@gmail.com>
2025-10-14 20:01:08 +0530

    web configs

---
+++ src/main/java/com/democode/soapApp/config/WebServiceConfig.java
@@ -0,0 +1,38 @@
+package com.democode.soapApp.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class WebServiceConfig {
+
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet> soapMessageDispatcherServlet(ApplicationContext context) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(context);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+
+    @Bean(name = "students")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema studentsSchema) {
+        DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+        definition.setPortTypeName("StudentsPort");
+        definition.setTargetNamespace("http://www.democode.com/soapApp/wsdl");
+        definition.setLocationUri("/ws");
+        definition.setSchema(studentsSchema);
+        return definition;
+    }
+
+    @Bean
+    public XsdSchema studentsSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("students.xsd"));
+    }
+}
