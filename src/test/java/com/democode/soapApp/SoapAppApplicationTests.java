3575ce7f04f902773ddee980d3929d0b6a888cfc
TheshikaSamaraweera <theshikanavod@gmail.com>
2025-10-14 20:01:41 +0530

    create main

---
+++ src/test/java/com/democode/soapApp/SoapAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.democode.soapApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
